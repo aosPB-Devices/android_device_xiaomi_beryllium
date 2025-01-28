b19d1ac6a5792b6660c8cb7e94041d6d0eb4f76a
Naruto-242 <Naruto242@proton.me>
2025-01-28 20:30:52 +0530

    beryllium: Initial aosPB bringup

--- aospb_beryllium.mk
+++ aospb_beryllium.mk
@@ -6,15 +6,20 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosPB stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := aospb_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Flags for aosPB
+CUSTOM_BUILDTYPE := OFFICIAL
+IS_SIGNED := true
+CUSTOM_MAINTAINER := PaperBoy
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
